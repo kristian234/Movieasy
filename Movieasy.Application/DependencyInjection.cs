2285754573b1b11a7b03e1c0c3a4ba10b6788a9d
Kristian <kristianteodosiev4@abv.bg>
2024-02-27 16:38:35 +0200

    MediatR configuration in the application layer has been setup

---
+++ Movieasy.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Movieasy.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+
+            
+
+            return services;
+        }
+    }
+}
