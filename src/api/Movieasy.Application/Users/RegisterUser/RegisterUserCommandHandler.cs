8739beac1a1a306a69bf9d1e20b0c1a0ede6ef06
Kristian <kristianteodosiev4@abv.bg>
2024-04-06 12:55:16 +0300

    accidentally deleted an important line of code on last commit

--- src/api/Movieasy.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
+++ src/api/Movieasy.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -33,7 +33,8 @@ namespace Movieasy.Application.Users.RegisterUser
                 request.Password,
                 cancellationToken);
 
- 
+            user.SetIdentityId(identityId);
+
             await _userRepository.AddAsync(user);
 
             await _unitOfWork.SaveChangesAsync();
