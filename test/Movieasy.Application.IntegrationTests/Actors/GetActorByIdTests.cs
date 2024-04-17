7c5526e51b02e3c5f27e2383f7af07336d2202e0
Kristian <kristianteodosiev4@abv.bg>
2024-04-17 14:58:41 +0300

    Update to the integration (primarily query tests)

--- test/Movieasy.Application.IntegrationTests/Actors/GetActorByIdTests.cs
+++ test/Movieasy.Application.IntegrationTests/Actors/GetActorByIdTests.cs
@@ -4,29 +4,20 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Movieasy.Application.Abstractions.Data;
 using Movieasy.Application.Actors.GetActorById;
+using Movieasy.Application.IntegrationTests.Infrastructure;
 using Movieasy.Domain.Actors;
 using Movieasy.Infrastructure;
 using NSubstitute;
 
 namespace Movieasy.Application.IntegrationTests.Actors
 {
-    public class GetActorByIdTests
+    public class GetActorByIdTests : BaseIntegrationTest
     {
         private readonly GetActorByIdQuery Query;
         private readonly GetActorByIdQueryHandler _handler;
 
-        private readonly IPublisher _publisherMock;
-        private readonly IApplicationDbContext _applicationDbContextMock;
-
         public GetActorByIdTests()
         {
-            _publisherMock = Substitute.For<IPublisher>();
-
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
-            _applicationDbContextMock = new ApplicationDbContext(options, _publisherMock);
-
             Query = new GetActorByIdQuery(Guid.NewGuid());
 
             _handler = new GetActorByIdQueryHandler(_applicationDbContextMock);
---
+++ test/Movieasy.Application.IntegrationTests/Infrastructure/BaseIntegrationTest.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Movieasy.Application.Abstractions.Data;
+using Movieasy.Infrastructure;
+using NSubstitute;
+
+namespace Movieasy.Application.IntegrationTests.Infrastructure
+{
+    public abstract class BaseIntegrationTest
+    {
+        protected readonly IPublisher _publisherMock;
+        protected IApplicationDbContext _applicationDbContextMock;
+
+        public BaseIntegrationTest()
+        {
+            _publisherMock = Substitute.For<IPublisher>();
+
+            var options = BuildDbContextOptions();
+            _applicationDbContextMock = new ApplicationDbContext(options, _publisherMock);
+        }
+
+        protected DbContextOptions<ApplicationDbContext> BuildDbContextOptions()
+        {
+            return new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                    .Options;
+        }
+    }
+}
